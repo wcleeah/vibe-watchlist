9d0b62b3cbfe81ca03f02557ac7ea7e6a8b1038b
Charlie.Lee <c050820021a@gmail.com>
2026-01-05 17:40:02 +0800

    Restore navbar horizontal padding
    - Added px-4 back to navigation container
    - Restores the preferred navbar spacing that looks better

--- components/navigation-tabs.tsx
+++ components/navigation-tabs.tsx
@@ -11,7 +11,7 @@ export function NavigationTabs() {
 
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 border-b border-gray-200 bg-background">
-      <div className="w-full">
+      <div className="w-full px-4">
         <div className="flex items-center justify-between h-16">
           <div className="flex space-x-8">
             <Link
