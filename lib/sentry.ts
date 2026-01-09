2198ff162741026eb4392a50c4d4c43aad50790e
Charlie.Lee <c050820021a@gmail.com>
2026-01-09 12:40:52 +0800

    bye bye sentry

--- lib/sentry.ts
+++
@@ -1,7 +0,0 @@
-export const sentryConfig = {
-    dsn: process.env.SENTRY_DSN!,
-    environment: process.env.SENTRY_ENVIRONMENT || 'development',
-    tracesSampleRate: parseFloat(
-        process.env.SENTRY_TRACES_SAMPLE_RATE || '0.1',
-    ),
-}
